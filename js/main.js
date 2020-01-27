66b4a80442b5d13c655a66ff944e1caca6449452
AfshinZehtab <AfshinZehtab@users.noreply.github.com>
2020-01-27 12:17:26 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-$(function() {
+(function() {
+    var nameValue = "";
+    $("input[id=Name]").change(function(){
+        nameValue = $(this).val();
+    });
+
+    $("#subttiles").hide();
+    $("button.showSub").on("click", function () {
+
+        var list = $('#sub-list li:gt(4)');
+        list.hide();
+        $("#subttiles").show();
+        $('a#myList-toggle').on("click", function() {
+            //list.slideToggle(400);
+            list.show();
+            $(this).remove();
+            return false;
+        });
+
+    });
 
 });
